f003f9471645c3765f5f505d06d35b6852808b67
John Cote <jwcotejr@users.noreply.github.com>
2019-12-23 15:34:18 -0500

    Update make.py

--- game/make.py
+++ game/make.py
@@ -1,7 +1,12 @@
+assert not __debug__  # Run with -OO
+
 import os
 
 FUNNY_FARM_SRC_DIR = 'Toontowns-Funny-Farm/'
 PANDA3D_DIR = 'C:\\Panda3D-1.10.4.1-x64'
 
 if not os.path.exists(FUNNY_FARM_SRC_DIR):
-    raise FileNotFoundError('Unable to locate the Toontown\'s Funny Farm game source code! Make sure to clone the Toontown\'s Funny Farm game source code repository into this directory!')
+    raise FileNotFoundError('Unable to locate the Toontown\'s Funny Farm game source code! Make sure to clone the Toontown\'s Funny Farm game source code repository into this directory.')
+
+if not os.path.exists(PANDA3D_DIR):
+    raise FileNotFoundError('Unable to locate the Panda3D SDK! Make sure to install the Panda3D SDK linked in the game source code repository\'s README.')
