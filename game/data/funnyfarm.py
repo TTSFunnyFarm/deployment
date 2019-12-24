4043625502eef0c9ce17096dc0e71635516eb983
John Cote <jwcotejr@users.noreply.github.com>
2019-12-23 22:32:29 -0500

    runtime: resources stuff

--- game/data/funnyfarm.py
+++ game/data/funnyfarm.py
@@ -1,7 +1,9 @@
-from panda3d.core import loadPrcFileData
+from panda3d.core import loadPrcFileData, Multifile
 from cryptography.fernet import Fernet
 import gamedata
+import glob
 
+# Config
 configKey = gamedata.CONFIG[0:45]
 configData = gamedata.CONFIG[45:]
 
@@ -10,4 +12,17 @@ config = configFernet.decrypt(configData)
 
 loadPrcFileData('game config', config.decode())
 
+# Resources
+for file in glob.glob('resources/*.mf'):
+    mf = Multifile()
+    mf.openReadWrite(Filename(file))
+    names = mf.getSubfileNames()
+    for name in names:
+        ext = os.path.splitext(name)[1]
+        if ext not in ['.jpg', '.jpeg', '.ogg', '.rgb']:
+            mf.removeSubfile(name)
+
+    vfs.mount(mf, Filename('/'), 0)
+
+# Time to shine, Funny Farm!
 import toontown.toonbase.FunnyFarmStart
